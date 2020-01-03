dccf8d44824072a55acdbd15f152fd33242f7483
Evan Banyash <ebanyash@gmail.com>
2020-01-03 02:14:40 -0800

    Joker's Trick

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Joker's Trick
+1. `yarn`
+2. `yarn global add pm2`
+3. `pm2 install typescript`
+4. `yarn run start`
+
+"Look inside trickstate.json to inspect the application state." -Joker
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import fs = require("fs")
+import path = require("path")
+import util = require("util")
+import moment from "moment"
+import Twitter = require("twitter-lite")
+import config = require("../config.json")
+
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
+interface TrickState {
+    nextIndex: number,
+    nextTrickTime: number | null,
+    mode: "sequential" | "random",
+    count: number
+}
+
+const dataDir = path.resolve(__dirname, "../data")
+
+const client = new Twitter({
+    consumer_key: config.apiKey,
+    consumer_secret: config.apiSecretKey,
+    access_token_key: config.accessToken,
+    access_token_secret: config.accessTokenSecret
+})
+
+async function loadTrickState(): Promise<TrickState> {
+    try {
+        return JSON.parse(
+            await readFile(path.resolve(dataDir, "trickstate.json"), {encoding: "utf-8"})
+        )
+    }
+    catch (e) {
+        return {nextIndex: 0, nextTrickTime: null, mode: "sequential", count: 0}
+    }
+}
+
+async function loadTricks(): Promise<string[]> {
+    const trickText = await readFile(path.resolve(__dirname, "..", config.trickFile), {encoding: "utf-8"})
+    if (trickText.includes("\r\n")) {
+        return trickText.split("\r\n\r\n\r\n")
+    }
+    return trickText.split("\n\n\n")
+}
+
+async function saveTrickState(state: TrickState) {
+    await writeFile(
+        path.resolve(dataDir, "trickstate.json"),
+        JSON.stringify(state, undefined, 2),
+        {encoding: "utf-8"}
+    )
+}
+
+async function init() {
+    const trickState = await loadTrickState()
+    setInterval(() => {
+        if (trickState.nextTrickTime === null || moment().valueOf() > trickState.nextTrickTime) {
+            trick(trickState)
+        }
+    }, 1000)
+}
+
+async function trick(state: TrickState) {
+    const tricks = await loadTricks() //reload tricks each trick for hot-editing
+
+    // if we're in random mode and some new tricks have been added
+    // switch to sequential mode and pick up at the next trick
+    if (state.mode == "random" && state.count < tricks.length) {
+        state.mode = "sequential"
+        state.nextIndex = state.count
+    }
+
+    console.log(tricks[state.nextIndex])
+    //await client.post("statuses/update", {status: tricks[state.nextIndex], source: "Joker's Trick"})
+    
+
+    // switch to random mode if we run out of tricks, or only one was added
+    if (state.mode == "sequential" && state.nextIndex + 1 >= tricks.length) {
+        state.mode = "random"
+    }
+    
+    //advance trick index according to mode
+    if (state.mode == "random") {
+        state.nextIndex = Math.floor(Math.random() * tricks.length)
+    }
+    else {
+        state.nextIndex = state.nextIndex + 1
+    }
+
+    state.count = tricks.length
+    const [quantity, unit] = config.interval.split(" ")
+
+    state.nextTrickTime = moment().add(Number(quantity), unit as any).valueOf()
+    await saveTrickState(state)
+}
+
+init()
